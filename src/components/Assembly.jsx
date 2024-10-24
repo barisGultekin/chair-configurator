e30fbec1731c95a085c7bb6de1a8d2c0c4731525
Ali Barış Gültekin <alibarisgultekin@gmail.com>
2024-10-24 19:40:32 +0300

    Update position prop

--- src/components/Assembly.jsx
+++ src/components/Assembly.jsx
@@ -5,7 +5,7 @@ import { useGLTF } from '@react-three/drei'
 const Assembly = (props) => {
   const { nodes, materials } = useGLTF('/models/assembly.gltf')
   return (
-    <group {...props} dispose={null}>
+    <group {...props} dispose={null} position={[0, -5, 0]}>
       <mesh geometry={nodes.Body1.geometry} material={materials['Steel_-_Satin']} />
       <mesh geometry={nodes.Body11.geometry} material={materials['Steel_-_Satin']} />
       <mesh geometry={nodes.Body12.geometry} material={materials['Steel_-_Satin']} />
