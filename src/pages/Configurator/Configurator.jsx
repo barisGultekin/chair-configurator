118592e99f37c970af62b2392b435f7c9dd6a2dc
Ali Barış Gültekin <alibarisgultekin@gmail.com>
2024-10-24 19:39:59 +0300

    Create configurator page layout

--- src/pages/Configurator/Configurator.jsx
+++ src/pages/Configurator/Configurator.jsx
@@ -1,39 +1,37 @@
-/* eslint-disable react/no-unknown-property */
-import { OrbitControls } from "@react-three/drei";
+import { Suspense, useRef } from "react";
+import { OrbitControls, Stage } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 import "./Configurator.scss";
 
-import { Canvas } from "@react-three/fiber";
-
-import Assembly from "../../components/Assembly";
+import Chair from "../../components/Chair";
+import ConfiguratorMenu from "./ConfiguratorMenu";
 
 const Configurator = () => {
+  const canvasRef = useRef();
+
   return (
     <div className="configurator">
-      <div className="canvas-box">
-        <Canvas camera={{ position: [10, 10, 10] }} shadows>
-          <mesh>
-            <ambientLight intensity={0.3} />
-            <directionalLight
-              position={[5, 5, 5]}
-              intensity={0.4}
-              castShadow
-              shadow-mapSize-width={1024}
-              shadow-mapSize-height={1024}
-            />
-
+      <div className="canvas-box" ref={canvasRef}>
+        <Canvas
+          camera={{ position: [40, 40, 100] }}
+          frameloop="demand"
+        >
+          <Suspense fallback={null}>
+            <Stage preset={"soft"} intensity={0} shadows="contact">
+              <Chair />
+            </Stage>
             <OrbitControls
               enablePan={false}
               enableZoom={true}
               maxPolarAngle={Math.PI / 2}
+              minDistance={50}
+              maxDistance={150}
             />
-            
-            <Assembly />
-          </mesh>
+          </Suspense>
         </Canvas>
       </div>
-      <div className="configurator-box">
-        <h1>config</h1>
-        <p>lorem ipsum dolor sit amet.</p>
+      <div className="config-section">
+        <ConfiguratorMenu />
       </div>
     </div>
   );
