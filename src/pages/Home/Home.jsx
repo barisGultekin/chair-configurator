c17b0b26923d6ee4c3c0dad8dc737867cea40f4a
Ali Barış Gültekin <alibarisgultekin@gmail.com>
2024-10-28 18:53:19 +0300

    Add placeholder sections to test navbar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,10 @@ import "./Home.scss";
 const Home = () => {
   return (
     <div className="home">
-
+      <section>1</section>
+      <section>2</section>
+      <section>3</section>
+      <section>4</section>
     </div>
   )
 }
