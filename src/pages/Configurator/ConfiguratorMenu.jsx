cd58b217d5e960341d68b526faf44b91d53dceab
Ali Barış Gültekin <alibarisgultekin@gmail.com>
2024-10-28 18:53:00 +0300

    Implement layout styling

--- src/pages/Configurator/ConfiguratorMenu.jsx
+++ src/pages/Configurator/ConfiguratorMenu.jsx
@@ -65,212 +65,229 @@ const ConfiguratorMenu = () => {
   };
 
   return (
-    <div className="menu">
-      <div className="header">
-        <h1>Configure Your Chair</h1>
-        <p>Design a chair for your personal needs.</p>
-      </div>
-
-      {isMobile && (
-        <div className="stage-navigation-wrapper">
-          <div className="stage-navigation">
-            <button onClick={handlePrevStage} disabled={currentStage === 0}>
-              <FaChevronLeft />
-            </button>
-            <span>{stages[currentStage]}</span>
-            <button
-              onClick={handleNextStage}
-              disabled={currentStage === stages.length - 1}
-            >
-              <FaChevronRight />
-            </button>
-          </div>
+    <div className="menu-wrapper">
+      <div className="menu">
+        <div className="header">
+          <h1>Configure Your Chair</h1>
+          <p>Design a chair for your personal needs.</p>
         </div>
-      )}
 
-      <div className="sections">
-        {(!isMobile || currentStage === 0) && (
-          <div className="section">
-            <div className="header">
-              <div className="section-title">
-                <h3>Presets</h3>
-                <button
-                  className={`section-toggle ${showPresets ? "open" : ""}`}
-                  onClick={() => setShowPresets(!showPresets)}
-                >
-                  <FaChevronDown />
-                </button>
-              </div>
-              <p>Pick a carefully designed preset.</p>
+        {isMobile && (
+          <div className="stage-navigation-wrapper">
+            <div className="stage-navigation">
+              <button onClick={handlePrevStage} disabled={currentStage === 0}>
+                <FaChevronLeft />
+              </button>
+              <span>{stages[currentStage]}</span>
+              <button
+                onClick={handleNextStage}
+                disabled={currentStage === stages.length - 1}
+              >
+                <FaChevronRight />
+              </button>
             </div>
-            {(isMobile || showPresets) && (
-              <div className="section-list-wrapper">
-                <div className="section-list text-list">
-                  <div
-                    className={`text-select ${
-                      currentPreset === presets.spaceShuttle ? "active" : ""
-                    }`}
-                    onClick={() => applyPreset(presets.spaceShuttle)}
+          </div>
+        )}
+
+        <div className="sections">
+          {(!isMobile || currentStage === 0) && (
+            <div className="section">
+              <div className="header">
+                <div className="section-title">
+                  <h3>Presets</h3>
+                  <button
+                    className={`section-toggle ${showPresets ? "open" : ""}`}
+                    onClick={() => setShowPresets(!showPresets)}
                   >
-                    Space Shuttle
-                  </div>
-                  <div
-                    className={`text-select ${
-                      currentPreset === presets.darkLeather ? "active" : ""
-                    }`}
-                    onClick={() => applyPreset(presets.darkLeather)}
-                  >
-                    Crest Prestige
-                  </div>
-                  <div
-                    className={`text-select ${
-                      currentPreset === presets.modernAlcantara ? "active" : ""
-                    }`}
-                    onClick={() => applyPreset(presets.modernAlcantara)}
-                  >
-                    Mojave
-                  </div>
-                  <div
-                    className={`text-select ${
-                      currentPreset === presets.racer ? "active" : ""
-                    }`}
-                    onClick={() => applyPreset(presets.racer)}
-                  >
-                    Grand Turismo
+                    <FaChevronDown />
+                  </button>
+                </div>
+                <p>Pick a carefully designed preset.</p>
+              </div>
+              {(isMobile || showPresets) && (
+                <div className="section-list-wrapper">
+                  <div className="section-list text-list">
+                    <div
+                      className={`text-select ${
+                        currentPreset === presets.spaceShuttle ? "active" : ""
+                      }`}
+                      onClick={() => applyPreset(presets.spaceShuttle)}
+                    >
+                      Space Shuttle
+                    </div>
+                    <div
+                      className={`text-select ${
+                        currentPreset === presets.darkLeather ? "active" : ""
+                      }`}
+                      onClick={() => applyPreset(presets.darkLeather)}
+                    >
+                      Crest Prestige
+                    </div>
+                    <div
+                      className={`text-select ${
+                        currentPreset === presets.modernAlcantara
+                          ? "active"
+                          : ""
+                      }`}
+                      onClick={() => applyPreset(presets.modernAlcantara)}
+                    >
+                      Mojave
+                    </div>
+                    <div
+                      className={`text-select ${
+                        currentPreset === presets.racer ? "active" : ""
+                      }`}
+                      onClick={() => applyPreset(presets.racer)}
+                    >
+                      Grand Turismo
+                    </div>
                   </div>
                 </div>
-              </div>
-            )}
-          </div>
-        )}
-
-        {(!isMobile || currentStage === 1) && (
-          <div className="section">
-            <h3>Armrests</h3>
-            <div className="section-list">
-              <div
-                className={`text-select ${
-                  armrestType === "solid" ? "active" : ""
-                }`}
-                onClick={() => {
-                  dispatch({ type: "SET_ARMREST_TYPE", payload: "solid" });
-                  setCurrentPreset(null);
-                }}
-              >
-                Solid
-              </div>
-              <div
-                className={`text-select ${
-                  armrestType === "cushioned" ? "active" : ""
-                }`}
-                onClick={() => {
-                  dispatch({ type: "SET_ARMREST_TYPE", payload: "cushioned" });
-                  setCurrentPreset(null);
-                }}
-              >
-                Cushioned
-              </div>
+              )}
             </div>
-          </div>
-        )}
+          )}
 
-        {(!isMobile || currentStage === 2) && (
-          <div className="section">
-            <h3>Cushion Material</h3>
-            <div className="section-list-wrapper">
-              <div className="section-list text-list">
-                {["Fabric", "MeshFabric", "Alcantara", "Leather"].map(
-                  (material) => (
-                    <div
-                      key={material}
-                      className={`text-select ${
-                        cushionMaterial === material ? "active" : ""
-                      }`}
-                      onClick={() => {
-                        dispatch({
-                          type: "SET_CUSHION_MATERIAL",
-                          payload: material,
-                        });
-                        setCurrentPreset(null);
-                      }}
-                    >
-                      {materialDisplayNames[material]}
-                    </div>
-                  )
-                )}
-              </div>
-            </div>
-          </div>
-        )}
-
-        {(!isMobile || currentStage === 3) && (
-          <div className="section">
-            <h3>Cushion Color</h3>
-            <div className="section-list scroll-list">
-              {availableCushionColors.map((color, index) => (
+          {(!isMobile || currentStage === 1) && (
+            <div className="section">
+              <h3>Armrests</h3>
+              <div className="section-list">
                 <div
-                  key={index}
-                  className={`color-select ${
-                    color === cushionColor ? "active" : ""
+                  className={`text-select ${
+                    armrestType === "solid" ? "active" : ""
                   }`}
                   onClick={() => {
-                    dispatch({ type: "SET_CUSHION_COLOR", payload: color });
+                    dispatch({ type: "SET_ARMREST_TYPE", payload: "solid" });
                     setCurrentPreset(null);
                   }}
                 >
-                  <div className="color-select-indicator">
+                  Solid
+                </div>
+                <div
+                  className={`text-select ${
+                    armrestType === "cushioned" ? "active" : ""
+                  }`}
+                  onClick={() => {
+                    dispatch({
+                      type: "SET_ARMREST_TYPE",
+                      payload: "cushioned",
+                    });
+                    setCurrentPreset(null);
+                  }}
+                >
+                  Cushioned
+                </div>
+              </div>
+            </div>
+          )}
+
+          {(!isMobile || currentStage === 2) && (
+            <div className="section">
+              <h3>Cushion Material</h3>
+              <div className="section-list-wrapper">
+                <div className="section-list text-list">
+                  {["Fabric", "MeshFabric", "Alcantara", "Leather"].map(
+                    (material) => (
+                      <div
+                        key={material}
+                        className={`text-select ${
+                          cushionMaterial === material ? "active" : ""
+                        }`}
+                        onClick={() => {
+                          dispatch({
+                            type: "SET_CUSHION_MATERIAL",
+                            payload: material,
+                          });
+                          setCurrentPreset(null);
+                        }}
+                      >
+                        {materialDisplayNames[material]}
+                      </div>
+                    )
+                  )}
+                </div>
+              </div>
+            </div>
+          )}
+
+          {(!isMobile || currentStage === 3) && (
+            <div className="section">
+              <h3>Cushion Color</h3>
+              <div className="section-list-wrapper">
+                <div className="section-list centered">
+                  {availableCushionColors.map((color, index) => (
                     <div
-                      className="color-select-dot"
-                      style={{ backgroundColor: color.color }}
-                    ></div>
-                  </div>
-                  <div className="color-select-label">{color.name}</div>
+                      key={index}
+                      className={`color-select ${
+                        color === cushionColor ? "active" : ""
+                      }`}
+                      onClick={() => {
+                        dispatch({ type: "SET_CUSHION_COLOR", payload: color });
+                        setCurrentPreset(null);
+                      }}
+                    >
+                      <div className="color-select-indicator">
+                        <div
+                          className="color-select-dot"
+                          style={{ backgroundColor: color.color }}
+                        ></div>
+                      </div>
+                      <div className="color-select-label">{color.name}</div>
+                    </div>
+                  ))}
                 </div>
-              ))}
-            </div>
-          </div>
-        )}
-
-        {(!isMobile || currentStage === 4) && (
-          <div className="section">
-            <h3>Structure</h3>
-            <div className="section-list">
-              <div
-                className={`img-select ${
-                  bodyTexture === "aluminum" ? "active" : ""
-                }`}
-                onClick={() => {
-                  dispatch({ type: "SET_BODY_TEXTURE", payload: "aluminum" });
-                  setCurrentPreset(null);
-                }}
-              >
-                <div className="img-select-indicator">
-                  <div className="img-select-dot">
-                    <img src="/assets/aluminum.webp" alt="Aluminum" />
-                  </div>
-                </div>
-                <div className="img-select-label">Aluminum</div>
-              </div>
-
-              <div
-                className={`img-select ${
-                  bodyTexture === "carbon" ? "active" : ""
-                }`}
-                onClick={() => {
-                  dispatch({ type: "SET_BODY_TEXTURE", payload: "carbon" });
-                  setCurrentPreset(null);
-                }}
-              >
-                <div className="img-select-indicator">
-                  <div className="img-select-dot">
-                    <img src="/assets/carbon-fiber.jpg" alt="Carbon Fiber" />
-                  </div>
-                </div>
-                <div className="img-select-label">Carbon Fiber</div>
               </div>
             </div>
-          </div>
-        )}
+          )}
+
+          {(!isMobile || currentStage === 4) && (
+            <div className="section">
+              <h3>Structure</h3>
+              <div className="section-list-wrapper">
+                <div className="section-list centered">
+                  <div
+                    className={`img-select ${
+                      bodyTexture === "aluminum" ? "active" : ""
+                    }`}
+                    onClick={() => {
+                      dispatch({
+                        type: "SET_BODY_TEXTURE",
+                        payload: "aluminum",
+                      });
+                      setCurrentPreset(null);
+                    }}
+                  >
+                    <div className="img-select-indicator">
+                      <div className="img-select-dot">
+                        <img src="/assets/aluminum.webp" alt="Aluminum" />
+                      </div>
+                    </div>
+                    <div className="img-select-label">Aluminum</div>
+                  </div>
+
+                  <div
+                    className={`img-select ${
+                      bodyTexture === "carbon" ? "active" : ""
+                    }`}
+                    onClick={() => {
+                      dispatch({ type: "SET_BODY_TEXTURE", payload: "carbon" });
+                      setCurrentPreset(null);
+                    }}
+                  >
+                    <div className="img-select-indicator">
+                      <div className="img-select-dot">
+                        <img
+                          src="/assets/carbon-fiber.jpg"
+                          alt="Carbon Fiber"
+                        />
+                      </div>
+                    </div>
+                    <div className="img-select-label">Carbon Fiber</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
