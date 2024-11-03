981e57dcdb8166421a3f07beb71c863f52a18d85
Ali Barış Gültekin <alibarisgultekin@gmail.com>
2024-11-03 21:48:39 +0300

    Create footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>Copyright ® 2024, Ali Baris Gultekin</p>
+    </div>
+  )
+}
+
+export default Footer
