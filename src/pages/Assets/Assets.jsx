5c7c6cccfdd0f6bf5adfdb267e62d2ed3986d28d
Ali Barış Gültekin <alibarisgultekin@gmail.com>
2024-12-03 15:00:28 +0300

    Implement downloading public files functionality

--- src/pages/Assets/Assets.jsx
+++ src/pages/Assets/Assets.jsx
@@ -4,17 +4,29 @@ import "./Assets.scss";
 const Assets = () => {
   return (
     <div className="assets">
-      <div className="assets-list">
-        <h1>Assets</h1>
-        <div className="asset-section">
-          <h3>Chair Model</h3>
-          <div className="download-button">GLTF</div>
-          <div className="download-button">OBJ</div>
+      <div className="assets-content">
+        <div className="assets-header">
+          <h1>Assets</h1>
+          <p>You can download the assets used for this projects from here.</p>
         </div>
-        <div className="asset-section">
-          <h3>Textures</h3>
-          <div className="download-button">Cushion Textures</div>
-          <div className="download-button">Structure Textures</div>
+
+        <div className="assets-list">
+          <div className="asset-section">
+            <h3>Chair Model</h3>
+            <a href={`${import.meta.env.BASE_URL}assets/models/chair.gltf`} download className="download-button">
+              Download GLTF
+            </a>
+          </div>
+          <div className="asset-section">
+            <h3>Textures</h3>
+            <div className="asset-subsection">
+              <p className="subsection-title">CUSHION TEXTURES</p>
+              <a href={`${import.meta.env.BASE_URL}assets/textures/downloads/alcantara.zip`}  download className="download-button">
+                Download Alcantara
+              </a>
+            </div>
+            <div className="asset-subsection">Structure Textures</div>
+          </div>
         </div>
       </div>
 
